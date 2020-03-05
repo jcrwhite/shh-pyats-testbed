cd85a3e2da087b7e8b7027c4b0271173856497ce
Josh White <coding.in.the.clouds@gmail.com>
2020-03-04 20:18:59 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shh-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'shh-pyats-testbed';
+}
