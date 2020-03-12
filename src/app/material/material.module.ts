3c4918f50068ae07af1b379e1a2400303cbaf7fa
Josh White <coding.in.the.clouds@gmail.com>
2020-03-11 20:07:28 -0400

    add copy-to-clipboard functionality

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,8 +5,12 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { ClipboardModule } from '@angular/cdk/clipboard';
 
 const exports = [
+  ClipboardModule,
+  MatIconModule,
   MatCardModule,
   MatButtonModule,
   MatFormFieldModule,
